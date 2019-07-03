183cd1029e5e55fb8c82d20db762c1241a4cef33
Y0lan <yolan@protonmail.ch>
2019-07-03 15:08:20 +1100

    Creating navbar + updating gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -7,8 +7,10 @@ define('DB_PASS', 'root');
 define('DB_NAME','database_name');
 
 // App Root
-define ('APPROOT', dirname(dirname(__FILE__)));
+define('APPROOT', dirname(dirname(__FILE__)));
 // Public Root
-define ('PUBLICROOT', dirname(dirname(dirname(__FILE__))). '/public/');
+define('PUBLICROOT', dirname(dirname(dirname(__FILE__))). '/public/');
+// Url Root
+define('URLROOT', 'http://192.168.33.10');
 // Site Name
 define('SITENAME', 'my-health-cloud');
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -23,4 +23,6 @@
     <title><?php echo SITENAME ?></title>
 </head>
 <body>
+<?php require APPROOT . '/views/inc/navbar.php';?>
+<div class="container">
 
